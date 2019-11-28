736186a04403e3c577af325929329ebecde6a7a4
Francis Tosolini <francis.tosolini@gmail.com>
2019-11-28 23:54:34 +0100

    remove keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCcSsbRBNYvPINEYAF4iJRvF51ia1PUi6s",
-    authDomain: "ftosolini-dad3e.firebaseapp.com",
-    databaseURL: "https://ftosolini-dad3e.firebaseio.com",
-    projectId: "ftosolini-dad3e",
-    storageBucket: "ftosolini-dad3e.appspot.com",
-    messagingSenderId: "899135860496",
-    appId: "1:899135860496:web:30aadaa507a4e0d7867f95",
-    measurementId: "G-MPFNREXPGK"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxxxxxxx.firebaseio.com",
+    projectId: "xxxxxx-xxx",
+    storageBucket: "xxxxxxxx-xxx.appspot.com",
+    messagingSenderId: "00000000",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "X-XXXXXXX"
   }
 };
 
